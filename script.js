e14cf3d5be8462bce1c837ee6edb22d1dfeec1c1
DayVee <dreyimbaev02@gmail.com>
2024-01-31 12:15:45 +0500

    ls 24

---
+++ script.js
@@ -0,0 +1,21 @@
+var userName = prompt('Введите своё имя');
+console.log('Меня зовут ' + userName);
+
+var userAge = prompt('Введите свой возраст');
+console.log('Мне ' + userAge);
+
+var userAnswer1 = prompt('10 + 3 = ?');
+console.log('Пример 1: 10 + 3 = 13 Ваш ответ ' + userAnswer1);
+
+var userAnswer2 = prompt('21 - 5 = ?');
+console.log('Пример 2: 21 - 5 = 16 Ваш ответ ' + userAnswer2);
+
+var userAnswer3 = prompt('16 * 2 = ?');
+console.log('Пример 3: 16 * 2 = 32 Ваш ответ ' + userAnswer3);
+
+var userAnswer4 = prompt('10 / 5 = ?');
+console.log('Пример 4: 10 / 5 = 2 Ваш ответ ' + userAnswer4);
+
+var userAnswer5 = prompt('11 % 5 = ?');
+console.log('Пример 5: 11 % 5 = 1 Ваш ответ ' + userAnswer5);
+alert('Откройте консоль для просмотра');
